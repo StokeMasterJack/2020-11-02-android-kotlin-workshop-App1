ca51960b784c51a46e77deb3f69a67a5d865da47
David Ford <dford@smart-soft.com>
2020-11-03 14:46:06 -0800

    Tue - 2:45

--- app/src/main/java/akw/app1/MainActivity.kt
+++ app/src/main/java/akw/app1/MainActivity.kt
@@ -1,17 +1,49 @@
 package akw.app1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Blackjack()
+        }
     }
 }
 
 @Composable
-fun Blackjack(){
-    Text(text = "Blackjack")
+fun Blackjack() {
+
+    Column(modifier = Modifier.padding(all = 10.dp)) {
+        Text(text = "Blackjack")
+
+        Row {
+            Button(onClick = {}) {
+                Text(text = "Deal")
+            }
+            Button(onClick = {}) {
+                Text(text = "Hit")
+            }
+            Button(onClick = {}) {
+                Text(text = "Stay")
+            }
+        }
+
+        Row {
+            Text(text = "Player Hand")
+            Text(text = "Dealer Hand")
+        }
+
+        Text(text = "Game Message")
+    }
 }
